a8f92f2d20efca7917b4fd42a9938652f9661cb5
Maryia Kunkel <maryiakunkel@gmail.com>
2023-07-23 08:16:38 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+console.log("Let's get this party started!");
+let gifData = undefined;
+async function searchGifs(val) {
+    const res = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${val}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    // console.log(res);
+
+    gifData = res.data.data.forEach(data => {
+        // console.log(data.url)
+    });
+    let url = res.data.data[0].url;
+    console.log(url)
+
+    $('#giphy').append(`<img src="${url}">`)
+};
+// `url="${res.data.data[0].url}"`
+$('.searchBtn').on('click', function () {
+    const input = $('#search').val();
+    searchGifs(input);
+    $('#search').val('')
+});
+
+
+$('.removeBtn').on('click', function () {
+    $('img').remove();
+})
+
+
+
+
+  
+// async function signUp(username, password, name) {
+// const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/signup', { user: { name, username, password } })
+// console.log(res);
+// }
+
+// async function login(username, password) {
+// const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/login', { user: { username, password } })
+// console.log(res);
+// return res.data.token;
+// }
+
+
+// async function getUsersWithAuth() {
+// const token = await login('butterschicken', '238197sadhj');
+// getUsers(token);
+// }
+
+// async function createStory() {
+// const token = await login('butterschicken', '238197sadhj');
+// const newStory = {
+//     token,
+//     story: {
+//     author: 'Butters',
+//     title: 'BOCK BOCK BOCK',
+//     url: 'http://chickens4lyfe.com'
+//     }
+// }
+// const res = await axios.post('https://hack-or-snooze-v3.herokuapp.com/stories', newStory);
+// console.log(res);
+// }
+
+// createStory();
+  
+  
+  
+  
+  
